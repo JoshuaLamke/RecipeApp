464cf3497f23e8a7b9295b46449dc5ca0efd902e
Joshua Lamke <joshua.lamke@gmail.com>
2021-01-03 17:05:57 -0500

    edit recipe name fix

--- recipe_app_frontend/scripts/view-recipe.js
+++ recipe_app_frontend/scripts/view-recipe.js
@@ -33,9 +33,7 @@ fetch('https://recipe-app-jg.herokuapp.com/api/recipes', {
         servingsElement.textContent = "Number of servings: " + wantedRecipe.servingAmount
         var str = wantedRecipe.ingredients
         var res = str.split(",");
-        for(ing in res) {
-            ing.trim()
-        }
+        res.filter((ing) => ing.trim() !== "")
         let count = 0;
         res.forEach((element) => {
             count++
@@ -45,9 +43,7 @@ fetch('https://recipe-app-jg.herokuapp.com/api/recipes', {
         });
         var strDir = wantedRecipe.directions
         var resDir = strDir.split(".");
-        for(dir in resDir) {
-            dir.trim()
-        }
+        resDir.filter((dir) => dir.trim() !== "")
         const resDirLength = resDir.length;
         let countDir = 0;
         resDir.forEach((element) => {
