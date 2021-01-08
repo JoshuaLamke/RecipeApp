39ca9c58b750c4d3113ba2dc9d5a0f85a4eb437b
Gerardo Abaunza <56178845+Gerardo-cmd@users.noreply.github.com>
2021-01-08 02:27:14 -0500

    Change Name Page

---
+++ recipe_app_frontend/scripts/name-change.js
@@ -0,0 +1,34 @@
+document.querySelector('#name-change-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newName = {
+        name: ''
+    }
+    newName = e.target[0].value
+    var userToken = localStorage.getItem("token")
+    
+    fetch("https://recipe-app-jg.herokuapp.com/api/user/update", {
+        method: 'POST',
+        headers: {
+            "Authorization": "Bearer " + userToken,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newName)
+    }).then((response) => {
+        if(response.status === 404) {
+            alert('Updating went wrong');
+            return;
+        }
+        response.json().then(function(data1) {
+            console.log(data1)
+            debugger
+            location.assign(`/user.html`)
+        })
+    }).catch((err) => { 
+        alert(err, " Could not find user with that login information, please try again or sign up to create a new account!")
+    })
+})
+
+document.querySelector('#cancel-button').addEventListener('click', (e) => {
+    e.preventDefault()
+    location.assign('/settings.html')
+})
--- recipe_app_frontend/scripts/settings.js
+++ recipe_app_frontend/scripts/settings.js
@@ -2,3 +2,8 @@ document.querySelector('#return-button').addEventListener('click', (e) => {
     e.preventDefault()
     location.assign('/user.html')
 })
+
+document.querySelector('#change-name-button').addEventListener('click', (e) => {
+    e.preventDefault()
+    location.assign('/name-change.html')
+})
